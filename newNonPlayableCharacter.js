63bc26fa1c53759df7fc22bc793432b828c9fb62
franc <fmenendezwork@gmail.com>
2023-05-11 20:34:31 -0700

    finished assignment along with class

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,24 +23,32 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkNorth() {
-        direction = 'north'
-        element.src = `./assets/red-character/north.gif`
+    async function walkNorth(time) {
+        direction = 'north' //you're going this way
+        element.src = `./assets/red-character/north.gif` //this is how you look
+        await sleep(time) //walk for this amount of time
+        stop() //cease activity
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time)
+        stop()
     }
 
     function stop() {
@@ -56,4 +64,10 @@ function newNonPlayableCharacter(x, y) {
         walkSouth: walkSouth,
         stop: stop
     }
+}
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
 }
